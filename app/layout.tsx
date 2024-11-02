18af198978d2e4d547dcc82bdab96505c4becab7
Nikola Perica <np46120@oss.unist.hr>
2024-11-02 12:43:33 +0100

    added wrapper so sidebar is not visible after sign out

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,16 @@ import GlobalStyleProvider from "./providers/GlobalStyleProvider";
 import ContextProvider from "./providers/ContextProvider";
 import {
   ClerkProvider,
-  SignInButton,
   SignedIn,
   SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import { auth } from "@clerk/nextjs/server";
+  auth,
+} from "@clerk/nextjs";
 import NextTopLoader from "nextjs-toploader";
 
-const nunito = Nunito({ 
+const nunito = Nunito({
   weight: ["400", "500", "600", "700", "800"],
-  subsets: ["latin"] });
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,20 +24,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-
-  const { userId } = auth()
-
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
         <head>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
-          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
-          crossOrigin="anonymous" referrerPolicy="no-referrer" />
-          </head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+            integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+        </head>
         <body className={nunito.className}>
           <NextTopLoader
             height={2}
@@ -47,11 +47,13 @@ export default function RootLayout({
           />
           <ContextProvider>
             <GlobalStyleProvider>
-              { userId && <Sidebar /> }
-              <div className="w-full ">{children}</div>
+              <SignedIn>
+                <Sidebar />
+              </SignedIn>
+              <div className="w-full">{children}</div>
             </GlobalStyleProvider>
           </ContextProvider>
-          </body>
+        </body>
       </html>
     </ClerkProvider>
   );
